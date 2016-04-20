6ab17f962b071857826df1a2d83ac1f157deecaa
Alexandru <alex2august@gmail.com>
2016-04-20 11:25:01 +0300

    dpad

--- app/src/main/java/com/example/m17336/bt_to_pc/ControlsActivity.java
+++ app/src/main/java/com/example/m17336/bt_to_pc/ControlsActivity.java
@@ -5,10 +5,12 @@ import android.content.DialogInterface;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.cocosw.bottomsheet.BottomSheet;
@@ -20,106 +22,89 @@ import yuku.ambilwarna.AmbilWarnaDialog;
 
 public class ControlsActivity extends AppCompatActivity {
 
-    int prevColor = 0xff0000ff;
-    int redMask   = 0x00ff0000;
-    int greenMask = 0x0000ff00;
-    int blueMask  = 0x000000ff;
-    
+    CommunicationThread  com;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controls);
-
-        final Activity that = this;
-        final ToggleButton onoff = (ToggleButton) findViewById(R.id.toggleButton);
-        final SeekBar intensity = (SeekBar) findViewById(R.id.seekBar);
-
         MyApplication app = (MyApplication) getApplicationContext();
-        final CommunicationThread com = app.getCom();
+        com = app.getCom();
 
-        Button colorPicker = (Button) findViewById(R.id.colorPickerBtn);
-        final AmbilWarnaDialog dialog = new AmbilWarnaDialog(this, prevColor, false, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+        Button up = (Button) findViewById(R.id.up),
+                down = (Button) findViewById(R.id.down),
+                left = (Button) findViewById(R.id.left),
+                right = (Button) findViewById(R.id.right),
+                rotate = (Button) findViewById(R.id.rotate);
+
+        up.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onOk(AmbilWarnaDialog dialog, int color) {
-                // color is the color selected by the user.
-                int r = (color & redMask) >> 16;
-                int g = (color & greenMask) >> 8;
-                int b = (color & blueMask);
-                prevColor = color;
-                com.write("HUE " + r + " " + g + " " + b);
-
-                //  onoff.setBackgroundColor(Color.rgb(r, g, b));
-
-            }
-
-            @Override
-            public void onCancel(AmbilWarnaDialog dialog) {
-                // cancel was selected by the user
-            }
-        });
-        colorPicker.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialog.show();
+            public boolean onTouch(View v, MotionEvent event) {
+                Toast.makeText(getApplicationContext(), "UP", Toast.LENGTH_SHORT).show();
+                if( com!=null ) com.write("1");
+                return false;
             }
         });
 
-
-        onoff.setOnClickListener(new View.OnClickListener() {
+        down.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                if(onoff.isChecked())
-                    com.write("ON");
-                else
-                    com.write("OFF");
+            public boolean onTouch(View v, MotionEvent event) {
+                Toast.makeText(getApplicationContext(), "DOWN", Toast.LENGTH_SHORT).show();
+                if( com!=null ) com.write("2");
+                return false;
             }
         });
 
-        onoff.setOnLongClickListener(new View.OnLongClickListener() {
+        left.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public boolean onLongClick(View v) {
-                new BottomSheet.Builder(that).title(onoff.isChecked() ? "Turn OFF in" : "Turn ON in").sheet(R.menu.bottom_sheet).listener(new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        String action = onoff.isChecked() ? "OFF " : "ON ";
-                        switch (which) {
-                            case R.id.m5:
-                                com.write(action + "5");
-                                break;
-                            case R.id.m10:
-                                com.write(action + "10");
-                                break;
-                            case R.id.m15:
-                                com.write(action + "15");
-                                break;
-                            case R.id.m30:
-                                com.write(action + "30");
-                                break;
-                            case R.id.h1:
-                                com.write(action + "60");
-                                break;
-                        }
-                    }
-                }).show();
-                return true;
+            public boolean onTouch(View v, MotionEvent event) {
+                Toast.makeText(getApplicationContext(), "LEFT", Toast.LENGTH_SHORT).show();
+                if( com!=null ) com.write("3");
+                return false;
             }
         });
 
-        intensity.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        right.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                com.write("I "+progress);
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
+            public boolean onTouch(View v, MotionEvent event) {
+                Toast.makeText(getApplicationContext(), "RIGHT", Toast.LENGTH_SHORT).show();
+                if( com!=null ) com.write("4");
+                return false;
             }
         });
+
+        rotate.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Toast.makeText(getApplicationContext(), "ROTATE", Toast.LENGTH_SHORT).show();
+                if( com!=null ) com.write("5");
+                return false;
+            }
+        });
+    }
+
+    public void up (View v) {
+        Toast.makeText(getApplicationContext(), "UP", Toast.LENGTH_SHORT).show();
+        if( com!=null ) com.write("1");
+    }
+
+    public void down (View v) {
+        Toast.makeText(getApplicationContext(), "DOWN", Toast.LENGTH_SHORT).show();
+        if( com!=null ) com.write("2");
+    }
+
+    public void left (View v) {
+        Toast.makeText(getApplicationContext(), "LEFT", Toast.LENGTH_SHORT).show();
+        if( com!=null ) com.write("3");
+    }
+
+    public void right (View v) {
+        Toast.makeText(getApplicationContext(), "RIGHT", Toast.LENGTH_SHORT).show();
+        if( com!=null ) com.write("4");
+    }
+
+    public void rotate (View v) {
+        Toast.makeText(getApplicationContext(), "ROTATE", Toast.LENGTH_SHORT).show();
+        if( com!=null ) com.write("5");
     }
 }
